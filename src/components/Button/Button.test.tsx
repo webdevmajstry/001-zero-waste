b53d18575072ee60c6040c6b7d5e573926cbc064
Rmichal503 <rakmichal503@gmail.com>
2022-11-26 14:46:21 +0100

    improvment of Button, new tests, delete styles

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -4,13 +4,24 @@ import { Button } from './Button';
 
 describe('Button component', () => {
   it('renders Button', () => {
-    render(<Button variant={''} size={''} />);
+    render(<Button/>);
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
-
+  it('renders Button with Test text', ()=>{
+    render(<Button>Test</Button>);
+    expect(screen.getAllByRole('button',{name:'Test'})).toBeInTheDocument
+  });
+  it('renders Button with large variant prop', ()=>{
+    render(<Button variant='large'/>);
+    expect(screen.getByRole('button')).toHaveAttribute('variant', 'large')
+  })
+  it('renders Button with small variant prop', ()=>{
+    render(<Button variant='small'/>);
+    expect(screen.getByRole('button')).toHaveAttribute('variant', 'small')
+  })
   it('Handles `onClick` event', () => {
     const onClick = jest.fn();
-    render(<Button onClick={onClick} variant={''} size={''}>Click me</Button>);
+    render(<Button onClick={onClick}>Click me</Button>);
 
     const button = screen.getByRole('button');
     fireEvent.click(button);
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,20 +1,14 @@
 import { ButtonHTMLAttributes } from 'react';
 
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {variant?:string,size?:string};
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {variant?:string};
 
 export const Button = (props: ButtonProps) => {
     const {variant = 'default'} = props
-    let btnClass:string=''
-    switch (props.size) {
-        case 'small':
-            btnClass = `${variant} btn-small`;
-            break;
-        case 'large':
-            btnClass = `${variant} btn-large`;
-            break;
-        default:
-            btnClass = `${variant} btn`;
-            break;
+    const variantClasses:any = {
+        'small':'bg-primary-500 text-light-100 hover:bg-primary-400 focus:bg-primary-400 focus:ring-alert transition ease-in-out duration-300 rounded text-xs py-2 px-2 m-1',
+        'default':'bg-primary-500 text-light-100 hover:bg-primary-400 focus:bg-primary-400 focus:ring-alert transition ease-in-out duration-300 rounded text-base py-2 px-3 m-1',
+        'large':'bg-primary-500 text-light-100 hover:bg-primary-400 focus:bg-primary-400 focus:ring-alert transition ease-in-out duration-300 rounded text-lg py-3 px-5 m-1',
     }
-return(<button className={`${btnClass}`} {...props}>{props.children}</button>)
+
+return(<button className={`${variantClasses[variant]}`} {...props}>{props.children}</button>)
 }
