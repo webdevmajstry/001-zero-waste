df31d7818a066a1f9add9fe50399d967e191c003
Krzysztof Białek <krzysztof.bialek1@gmail.com>
2022-12-05 00:58:41 +0100

    Working component without tests

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,11 +1,32 @@
 import { Meta, Story } from '@storybook/react';
+
 import { Card } from './Card';
 
 export default {
   title: 'Card',
   component: Card,
+  argTypes: {
+    title: {
+      type: {
+        name: 'string',
+      },
+      defaultValue: 'Modern Picture',
+    },
+    optionalPicture: {
+      type: {
+        name: 'string',
+      },
+      defaultValue: '',
+    },
+    price: {
+      defaultValue: 15,
+      type: {
+        name: 'number',
+      },
+    },
+  },
 } as Meta;
 
 const Template: Story = (args) => <Card {...args} />;
 
-export const Primary = Template.bind({});
+export const CardComponent = Template.bind({});
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,22 +1,66 @@
+import ShoppingCart from '@svg/shopping-cart.svg';
+import Image from 'next/image';
 import React from 'react';
 
-import Icon from '../../assets/cartIcon.svg';
+import DefaultImage from './DefaultImage.png';
 
-// That code is temporary :)
+export type CardProps = {
+  children?: React.ReactNode;
+  title?: string;
+  price?: number;
+  heading?: string;
+  optionalPicture?: string;
+  footer?: React.ReactNode;
+};
 
-export const Card = (props: any) => (
-  <div {...props} className="w-52 rounded-lg shadow-md">
-    <div className="image h-48 w-full rounded-t-lg  bg-dark-200"></div>
-    <div className="content flex flex-col py-2 px-4">
-      <div className="flex justify-between mb-4">
-        <p>Modern Picture</p>
-        <div className="action">
-          <Icon />
+export const Card = ({
+  optionalPicture,
+  heading,
+  title,
+  price,
+  children,
+  footer,
+}: CardProps) => {
+  return (
+    <>
+      {heading ? (
+        <div className="rounded-lg h-64 w-72 shadow-md p-8">
+          <div className="flex flex-col justify-between h-full">
+            <h1 className="font-bold text-x">{heading}</h1>
+            {children}
+            {footer}
+          </div>
         </div>
-      </div>
-      <div className="price">
-        <p className="font-bold">$15.00</p>
-      </div>
-    </div>
-  </div>
-);
+      ) : (
+        <div className="w-52 rounded-lg h-64 shadow-md">
+          <div className="relative h-40 w-full rounded-t-lg">
+            {optionalPicture ? (
+              <Image
+                src={optionalPicture}
+                className="rounded-t-lg"
+                fill
+                alt={'default photo'}
+              />
+            ) : (
+              <Image
+                src={DefaultImage}
+                className="rounded-t-lg"
+                fill
+                alt={'product image'}
+              />
+            )}
+          </div>
+          <div className="flex flex-col py-2 px-4">
+            <div className="flex justify-between mb-4">
+              <h1 className="font-light text-sm">{title}</h1>
+              <ShoppingCart />
+            </div>
+            <div className="price">
+              <p className="font-light text-xl">${price?.toFixed(2)}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
