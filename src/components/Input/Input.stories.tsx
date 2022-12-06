991630fd2f7f983ddce102f66a382a82fb55de85
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2022-12-06 16:00:21 +0100

    Updated stories for Input Component

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,20 +1,36 @@
-import { Meta } from '@storybook/react';
+import React, { useState } from 'react';
+import { Story, Meta } from '@storybook/react';
+import { Input, Props } from './Input';
 
-import Input from './Input';
-
-const meta: Meta = {
-  title: 'Input',
+export default {
+  title: 'General/Input',
   component: Input,
-};
+} as Meta;
 
-export default meta;
+const Template: Story<Props> = () => {    
+    const [localValue, setValue] = useState<string>('');
+    const onChangeInput = (inputValue: string) => {
+        setValue(inputValue);
+    }
+    return (
+        <><Input
+            labelText='Label'
+            placeholder='Placeholder'
+            className='test-class-name'
+            name='test-name'
+            value={localValue}
+            htmlFor='test-for'
+            onChangeInput={onChangeInput}
+            helperText='This is helperText'
+            />
+        </>
 
-export const Default = {
-  render: () => (
-    <>
-      <h1>{'email:'}</h1>
-      <input placeholder={'Type your email'}></input>
-      <p>{'Please provide valid email address'}</p>
-    </>
-  ),
-};
+    )
+}
+
+export const Default: Story<Props> = Template.bind({});
+Default.parameters = {
+  design: {
+    url: ''
+  }
+}
