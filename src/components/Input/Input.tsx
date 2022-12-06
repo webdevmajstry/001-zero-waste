d1885ccf526548b7ddecac05ee13e4b1e5b9ca6a
Emilia Buczek <101749808+Ebmili@users.noreply.github.com>
2022-12-06 16:01:28 +0100

    Updated input props.

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,19 +1,43 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React from 'react'
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  placeholder: string;
-  name: string;
-  HelperText: string;
+export type Props = {
+
+  labelText?: string,
+  placeholder?: string,
+  className?: string,
+  value?: string,
+  name?: string,
+  htmlFor?: string,
+  /**
+   * Callback for Input
+   */
+  onChangeInput: any,
+  text?: string,
+  helperText?: string,
 }
 
-const Input: FC<InputProps> = ({ placeholder, name, HelperText }) => {
-  return (
-    <div className="input">
-      <h1>name={name}</h1>
-      <input type="input" placeholder={placeholder}></input>
-      <p>HelperText={HelperText}</p>
-    </div>
-  );
-};
-
-export default Input;
+export function Input({labelText, placeholder='', className='', value='', name='', htmlFor='',helperText='', onChangeInput}:Props) {    
+    return (
+        <div>
+            <label className='text-text-01-normal font-medium text-sm leading-5'>
+                {labelText}
+            </label>
+            <div className="relative">
+                <input
+                    id={htmlFor}
+                    className={[
+                        "relative w-64 pl-3 h-8 py-2 border rounded bg-ui-03 border-ui-06 text-text-01-normal leading-5 text-sm text-left hover:bg-ui-03 hover:border-ui-07 focus:bg-ui-03 focus:outline-none focus:border-interactive-01-active active:bg-ui-03",                        
+                        className
+                        ].join(' ')} 
+                    placeholder={placeholder}
+                    value={value}
+                    name={name}
+                    onChange={ (e) => { onChangeInput(e.target.value) }}
+                />
+            </div>
+            <p className='text-text-01-normal font-medium text-sm leading-5'>
+                {helperText}
+            </p>
+        </div>
+    )
+}   
