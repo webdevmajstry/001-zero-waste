29a8edebf1933433ea2cd86156df8a5e74aca11e
Rmichal503 <rakmichal503@gmail.com>
2022-12-09 23:38:01 +0100

    tests for rating component

--- src/components/Rating/Rating.test.tsx
+++ src/components/Rating/Rating.test.tsx
@@ -1,13 +1,41 @@
-jest.mock('@svg/star.svg', ()=>'icon of star');
-jest.mock('@svg/heart.svg', ()=>'icon of heart');
-
 import { render, screen } from "@testing-library/react"
 import { Rating } from "./Rating"
 
 
 describe('Rating Component',()=>{
-    it('Renders image', ()=>{
-        render(<Rating rate={2} variant='hearts'/>)
-        expect(screen.getByText('icon of heart'))
+    it('Renders hearts', ()=>{
+        render(<Rating variant='hearts' rate={1}/>)
+        expect(screen.getAllByTestId('hearts rating'))
+    })
+    it('Renders stars', ()=>{
+        render(<Rating variant='stars' rate={1}/>)
+        expect(screen.getAllByTestId('stars rating'))
+    })
+    it('Renders x times', ()=>{
+        const ratings =4
+        render(<Rating variant='stars' rate={ratings}/>)
+        const ratingArr = screen.getAllByTestId('stars rating')
+        for(let i=0;i<ratings;i++){
+            expect(ratingArr[i]).toHaveClass('mx-px')
+        }
+        for(let i=ratings;i<ratings;i++){
+            expect(ratingArr[i]).toHaveClass('opacity-50')
+        }
+    })
+    it('Renders number of rating', ()=>{
+        render(<Rating type='number' rate={2}/>)
+        expect(screen.getAllByTestId('number of rating'))
+    })
+    it('Check hidden svg`s', ()=>{
+        const ratings =3
+        render(<Rating variant='stars' type="hidden" rate={ratings}/>)
+        const ratingArr = screen.getAllByTestId('stars rating')
+        for(let i=ratings;i<5;i++){
+            expect(ratingArr[i]).toHaveClass('hidden')
+        }
+    })
+    it('Not rendering', ()=>{
+        render(<Rating rate={0}/>)
+        expect(screen.queryAllByTestId('rating container')).toBeNull
     })
 })
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -13,36 +13,35 @@ export const Rating = (props: RatingProps) => {
   if (rate <= 0) return null;
   return (
     <div
+      data-testid='rating container'
       key={type + variant + rate * Math.random()}
       className="flex items-center"
     >
-      {[...Array(type === 'number' ? 1 : 5)].map(
-        (el, index) => {
-          const isHidden = type === 'hidden';
-          const isActive = index + 1 <= rate;
-          const iconClasses = clsx(
-            isActive && 'mx-px',
-            isHidden && !isActive && 'hidden',
-            !isActive && !isHidden && 'opacity-50',
-          );
-          return (
-            <>
-              {variant === 'hearts' ? (
-                <Heart
-                  key={`rate${variant}${Math.random()}`}
-                  className={iconClasses}
-                />
-              ) : (
-                <Stars
-                  key={`rate${variant}${Math.random()}`}
-                  className={iconClasses}
-                />
-              )}
-            </>
-          );
-        },
-      )}
-      {type === 'number' && <span>{rate}</span>}
+      {[...Array(type === 'number' ? 1 : 5)].map((el, index) => {
+        const isHidden = type === 'hidden';
+        const isActive = index + 1 <= rate;
+        const iconClasses = clsx(
+          isActive && 'mx-px',
+          isHidden && !isActive && 'hidden',
+          !isActive && !isHidden && 'opacity-50',
+        );
+        return (
+          <>
+            {variant === 'hearts' ? (
+              <Heart data-testid='hearts rating'
+                key={`rate${variant}${Math.random()}`}
+                className={iconClasses}
+              />
+            ) : (
+              <Stars data-testid='stars rating'
+                key={`rate${variant}${Math.random()}`}
+                className={iconClasses}
+              />
+            )}
+          </>
+        );
+      })}
+      {type === 'number' && <span data-testid='number of rating'>{rate}</span>}
     </div>
   );
 };
